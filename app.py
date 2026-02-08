a127eec079f6d3ecbc8a2e9656c3a6a9e6a3f672
yash-kumar-26 <yashkumar5588@gmail.com>
2026-02-08 20:34:57 +0530

    Added file upload feature using Flask

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route("/")
 def home():
-    return "Cloud File Storage Backend Running"
+    return '''
+     <h2>File Upload</h2>
+    <form method="POST" action="/upload" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" value="Upload">
+    </form>
+    '''
 
-if __name__ ==  "__main__":
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return "File uploaded successfully"
+    return "No file selected"
+
+if __name__ == "__main__":
     app.run(debug=True)

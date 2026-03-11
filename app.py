abb51dd24594eaaa0d907c34ab0551907497d192
yash-kumar-26 <yashkumar5588@gmail.com>
2026-03-11 21:15:10 +0530

    Integrate SQLite database to store file metadata and update file listing to use DB instead of folder scan

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask import send_from_directory
 import os
 import time
+import sqlite3
 
 app = Flask(__name__)
 
@@ -27,22 +28,43 @@ def upload_file():
         timestamp = str(int(time.time()))
         new_filename = timestamp + "_" + file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
+        conn = sqlite3.connect('database/files.db')
+        cursor = conn.cursor()
+
+        cursor.execute(
+        "INSERT INTO files (original_name, saved_name, upload_time) VALUES (?, ?, ?)",
+        (file.filename, new_filename, timestamp)
+    )
+
+        conn.commit()
+        conn.close()
         return "File uploaded successfully"
     return "No file selected"
 
 @app.route("/files")
 def list_files():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    conn = sqlite3.connect('database/files.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT original_name, saved_name FROM files")
+    files = cursor.fetchall()
+    
+    conn.close()
     
     output = "<h2>Uploaded Files</h2>"
     
     for file in files:
+        original_name = file[0]
+        saved_name = file[1]
+
         output += f'''
         <p>
-        <a href="/download/{file}">{file}</a> 
-        | <a href="/delete/{file}">Delete</a>
+        {original_name}
+        | <a href="/download/{saved_name}">Download</a>
+        | <a href="/delete/{saved_name}">Delete</a>
         </p>
         '''
+
     return output
 
 @app.route('/download/<filename>')
@@ -55,9 +77,34 @@ def delete_file(filename):
     
     if os.path.exists(file_path):
         os.remove(file_path)
+
+        conn = sqlite3.connect('database/files.db')
+        cursor = conn.cursor()
+
+        cursor.execute("DELETE FROM files WHERE saved_name = ?", (filename,))
+        conn.commit()
+        conn.close()
         return f"{filename} deleted successfully"
     
     return "File not found"
+
+def init_db():
+    conn = sqlite3.connect('database/files.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS files (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            original_name TEXT,
+            saved_name TEXT,
+            upload_time TEXT
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+init_db()
 
 
 if __name__ == "__main__":
